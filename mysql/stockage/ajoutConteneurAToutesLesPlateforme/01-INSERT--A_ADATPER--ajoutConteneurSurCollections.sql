b42bb1dc2995631b333e930c8d6a7ff6b2fd7b47
Corinne HUET <corinne.huet@sesan.fr>
2025-10-03 18:15:14 +0200

    Update 01-INSERT--A_ADATPER--ajoutConteneurSurCollections.sql

--- mysql/stockage/ajoutConteneurAToutesLesPlateforme/01-INSERT--A_ADATPER--ajoutConteneurSurCollections.sql
+++ mysql/stockage/ajoutConteneurAToutesLesPlateforme/01-INSERT--A_ADATPER--ajoutConteneurSurCollections.sql
@@ -1,8 +1,8 @@
 insert into CONTENEUR_BANQUE(conteneur_id, banque_id)
 select c.conteneur_id, b.banque_id from BANQUE b 
-	join CONTENEUR c on c.NOM in ('CRB Nord 4.44 CNG80 PRIMARY', 'CRB Nord 4.44 CNG80 BIS', 'CRB Nord 4.46 Ambiant', 'CRB Nord 4.44 CNG80 PROJETS EN COURS', 'CRB Nord 4.46 Frigo 5°C')
+	join CONTENEUR c on c.NOM in ('[NOM_CONTENEUR_1]', '[NOM_CONTENEUR_2]', ...)
 	left outer join CONTENEUR_BANQUE cb on cb.BANQUE_ID = b.BANQUE_ID and cb.conteneur_id = c.conteneur_id 
 where 
-	b.PLATEFORME_id in (select plateforme_id from PLATEFORME where nom = 'CRB NORD') 
+	b.PLATEFORME_id in (select plateforme_id from PLATEFORME where nom = '[NOM_PLATEOFMRE]') 
 	and b.archive = 0
 	and cb.conteneur_id is null;
